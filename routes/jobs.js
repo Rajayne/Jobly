f887add1760e4f13142a7221bcd330666ed10c65
Rajayne <ryann.j.nealey@gmail.com>
2023-05-02 08:15:23 -0700

    Add patch and delete job routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -14,6 +14,10 @@ const jobSearchSchema = require("../schemas/jobSearch.json");
 
 const router = new express.Router();
 
+/** POST / { job } => { job }
+ * job should be { title, salary, equity, companyHandle }
+ * Returns { id, title, salary, equity, companyHandle }
+ * Authorization required: admin */
 router.post("/", ensureAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, jobNewSchema);
@@ -29,6 +33,13 @@ router.post("/", ensureAdmin, async function (req, res, next) {
   }
 });
 
+/** GET / =>
+ *   { jobs: [ { id, title, salary, equity, companyHandle, companyName }, ...] }
+ * Can provide search filter in query:
+ * - minSalary
+ * - hasEquity (true returns only jobs with equity > 0, other values ignored)
+ * - title (will find case-insensitive, partial matches)
+ * Authorization required: none */
 router.get("/", async function (req, res, next) {
   const q = req.query;
   // convert strings to integers/boolean
@@ -49,10 +60,42 @@ router.get("/", async function (req, res, next) {
   }
 });
 
+/** GET /[jobId] => { job }
+ * Returns { id, title, salary, equity, company }
+ *   where company is { handle, name, description, numEmployees, logoUrl }
+ * Authorization required: none */
 router.get("/:id", async function (req, res, next) {
   try {
     const job = await Job.get(req.params.id);
     return res.json({ job });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PATCH /[id]  { fld1, fld2, ... } => { job }
+ * Authorization required: admin */
+router.patch("/:id", ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, jobUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const job = await Job.update(req.params.id, req.body);
+    return res.json({ job });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[id]  =>  { deleted: id }
+ * Authorization required: admin */
+router.delete("/:id", ensureAdmin, async function (req, res, next) {
+  try {
+    await Job.remove(req.params.id);
+    return res.json({ deleted: +req.params.id });
   } catch (err) {
     return next(err);
   }
