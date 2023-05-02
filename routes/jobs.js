3f40ace7d2bd798a0a8470da4c9f5fd0a963089f
Rajayne <ryann.j.nealey@gmail.com>
2023-05-02 08:05:16 -0700

    Add pos job route

--- routes/jobs.js
+++ routes/jobs.js
@@ -13,3 +13,18 @@ const jobUpdateSchema = require("../schemas/jobUpdate.json");
 const jobSearchSchema = require("../schemas/jobSearch.json");
 
 const router = new express.Router();
+
+router.post("/", ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, jobNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const job = await Job.create(req.body);
+    return res.status(201).json({ job });
+  } catch (err) {
+    return next(err);
+  }
+});
